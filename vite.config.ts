40019d3174dbd91d6a43bc6cd4a851ca7ef9b82a
zizan-azman <azizan.azman.fe@gmail.com>
2025-12-28 10:19:54 +0800

    rename base

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/zizan-azman/improve-cls/",
+  base: "/improve-cls/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
