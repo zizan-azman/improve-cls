7c08b3df116fb5aae0322195df0676226949caae
zizan-azman <azizan.azman.fe@gmail.com>
2025-12-30 04:42:46 +0800

    Implemented ScrollToTop

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Outlet } from "react-router-dom";
 import { SideNavigation } from "@/components/navbar/side-navigation/SideNavigation";
 import styles from "./App.module.scss";
 import { SIDE_NAVIGATION_MOCK } from "./mock/SideNavigation.mock";
+import ScrollToTop from "./components/scroll-to-top/ScrollToTop";
 
 function App() {
   return (
     <div className={styles["app-layout"]}>
       <SideNavigation config={SIDE_NAVIGATION_MOCK} />
       <main className={styles["app-layout__content"]}>
+        <ScrollToTop />
         <Outlet />
       </main>
     </div>
